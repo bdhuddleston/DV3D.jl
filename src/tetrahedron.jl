b9c7003a81e22580dd371e320c997279c0b05212
Bradley Huddleston <huddpitcher@gmail.com>
2018-10-30 18:11:11 -0500

    Expanded tetrahedron definition to include indices to neighbors

--- src/tetrahedron.jl
+++ src/tetrahedron.jl
@@ -1,27 +1,47 @@
 # tetrhedron.jl
 
-# TODO: Define functions to get faces from tets 
+using GeometryTypes
+using StaticArrays
 
-const Tetrahedron = SVector{4,Int}
+
+struct Tetrahedron
+    vertices::SVector{4,Int}
+    neighbors::MVector{4,Int}
+end
 
 function permute(tet::Tetrahedron, evenorodd::Int)::Tetrahedron
     if (evenorodd % 2) == 0
-        tet = Tetrahedron(tet[2],tet[1],tet[4],tet[3])
+        Tetrahedron([tet.vertices[2],tet.vertices[1],tet.vertices[4],tet.vertices[3]],
+                          [tet.neighbors[2],tet.neighbors[1],tet.neighbors[4],tet.neighbors[3]])
     else
-        tet = Tetrahedron(tet[4],tet[3],tet[2],tet[1])
+        Tetrahedron([tet.vertices[4],tet.vertices[3],tet.vertices[2],tet.vertices[1]],
+                          [tet.neighbors[4],tet.neighbors[3],tet.neighbors[2],tet.neighbors[1]])
     end
 end
 
 function rotate(tet::Tetrahedron, pivot::Int)::Tetrahedron
     if pivot == 1
-        Tetrahedron(tet[1], tet[4], tet[2], tet[3])
+        Tetrahedron([tet.vertices[1], tet.vertices[4], tet.vertices[2], tet.vertices[3]],
+                  [tet.neighbors[1],tet.neighbors[4],tet.neighbors[2],tet.neighbors[3]])
     elseif pivot == 2
-        Tetrahedron(tet[4], tet[2], tet[1], tet[3])
+        Tetrahedron([tet.vertices[4], tet.vertices[2], tet.vertices[1], tet.vertices[3]],
+                  [tet.neighbors[4],tet.neighbors[2],tet.neighbors[1],tet.neighbors[3]])
     elseif pivot == 3
-        Tetrahedron(tet[4], tet[1], tet[3], tet[2])
+        Tetrahedron([tet.vertices[4], tet.vertices[1], tet.vertices[3], tet.vertices[2]],
+                  [tet.neighbors[4],tet.neighbors[1],tet.neighbors[3],tet.neighbors[2]])
     elseif pivot == 4
-        Tetrahedron(tet[3], tet[1], tet[2], tet[4])
+        Tetrahedron([tet.vertices[3], tet.vertices[1], tet.vertices[2], tet.vertices[4]],
+                  [tet.neighbors[3],tet.neighbors[1],tet.neighbors[2],tet.neighbors[4]])
     else
         tet
+    end
+end
+
+function getface(tet::Tetrahedron, index::Int)::Face{3,Int}
+    face::Array{Int,1} = tet.vertices[setdiff(1:4,index)]
+    if index % 2 == 1
+        Face{3,Int}(face[1],face[3],face[2])
+    else
+        Face{3,Int}(face...)
     end
 end
